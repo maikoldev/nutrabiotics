f49ffb57e949100359cf511169112b27b1b55fc9
Jhon Michael Garcia <me@maikoldev.com>
2024-02-27 02:06:04 -0500

    2024/02/27 - Michael Garcia - AboutUs Responsive
    - Adding 'xs' Screen Size on Tailwind

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -8,6 +8,7 @@ import Breadcrumb from "@/components/layouts/Breadcrumb";
 import CustomButton from "@/components/custom-button";
 
 import BannerRes from "@/assets/about-us/banner-res.png"
+import Banner from "@/assets/about-us/banner.png"
 import FoundersImg from "@/assets/about-us/founders.jpg"
 import AuroraAImg from "@/assets/about-us/aurora-a.png"
 
@@ -30,10 +31,11 @@ const tabItems = [
 export default function Page() {
   return (
     <>
-      <section className="relative min-h-[75vh] flex items-end">
-        <Image className="w-full h-full object-cover" fill src={BannerRes} alt="AboutUs Banner" />
+      <section className="relative min-h-[75vh] sm:min-h-[65vh] flex items-end">
+        <Image className="w-full h-full object-cover sm:hidden" fill src={BannerRes} alt="AboutUs Banner" />
+        <Image className="w-full h-full object-cover max-sm:hidden" fill src={Banner} alt="AboutUs Banner" />
         <div className="container mx-auto relative z-10 mb-10">
-          <p className="text-white font-bold text-center">
+          <p className="text-white font-bold text-center lg:w-[65%] mx-auto">
             Somos una tribu liderada por médicos, que le ofrece a los profesionales de la salud y al mundo las herramientas funcionales para posibilitar y vivir el bienestar.
           </p>
         </div>
@@ -50,16 +52,16 @@ export default function Page() {
           <h2 className="text-title text-center mb-10">
             <span className="text-subtitle">Nuestros</span> <br /> Fundadores
           </h2>
-          <div className="relative rounded-2xl aspect-square overflow-hidden mb-6">
+          <div className="relative rounded-2xl max-sm:aspect-square overflow-hidden mb-6 sm:h-[45vw] xl:h-[36vw]">
             <div className="bg-purple-gradient w-full h-full relative z-10"></div>
-            <Image className="w-full h-full object-cover" fill src={FoundersImg} alt="Founders" />
-            <div className="absolute bottom-0 w-full grid grid-cols-3 justify-items-center gap-1 p-4 text-small font-bold text-light-yellow z-10">
+            <Image className="w-full h-full object-cover sm:object-top" fill src={FoundersImg} alt="Founders" />
+            <div className="absolute bottom-0 w-full grid grid-cols-3 justify-items-center gap-1 max-xs:px-3 p-4 max-md:text-small font-bold lg:text-subtitle-sm lg:underline text-light-yellow z-10">
               <span>Benoit Raby</span>
               <span>Luc Lemaire</span>
               <span>Javier Galvis</span>
             </div>
           </div>
-          <CustomButton block>Conoce sus biografías</CustomButton>
+          <CustomButton block className="md:hidden">Conoce sus biografías</CustomButton>
         </div>
       </section>
 
@@ -68,7 +70,7 @@ export default function Page() {
           <h2 className="text-title text-center mb-10">
             <span className="text-subtitle">La ciencia detrás</span> <br /> De Nutrabiotics
           </h2>
-          <p className="font-bold text-center">
+          <p className="font-bold text-center lg:w-[52%] mx-auto">
             Somos especialistas en Medicina funcional y Kinesiología aplicada (en otras palabras, es un “desenmarañador” de la fisiopatología).
           </p>
         </div>
@@ -77,55 +79,58 @@ export default function Page() {
       <section className="relative pt-20 py-8">
         <Image className="w-full absolute translate-y-[-20%] -z-10" src={AuroraAImg} alt="Aurora" />
         <div className="container mx-auto">
-          <h2 className="text-title text-center mb-10">
-            <span className="text-subtitle">Los 6 principios de</span> <br /> Nuestras <br /> creaciones
+          <h2 className="text-title text-center mb-10 lg:mb-16">
+            <span className="text-subtitle">Los 6 principios de</span> <br /> Nuestras <br className="sm:hidden" /> creaciones
           </h2>
 
-          <div className="w-full">
-            <div className="mx-auto w-full rounded-2xl p-2">
-              {tabItems.map((item, index) => (
-                <Disclosure key={`tab-item-${item.title}`} defaultOpen={index == 0 ? true : false}>
-                  {({ open }) => (
-                    <>
-                      <Disclosure.Button className={`${open ? 'bg-violet text-white px-6' : ''} w-full flex items-center rounded-full gap-6 py-3 transition-colors`}>
-                        <div className={`h-[60px] w-[60px] grid place-content-center rounded-full p-3x border ${open ? 'border-transparent' : 'border-purple'}`}>
-                          <ArrowDownIcon className={`${!open && '-rotate-90'} transition`} />
-                        </div>
-                        <span className={`text-normal ${open ? 'font-bold' : 'font-extrabold'}`}>{item.title}</span>
-                      </Disclosure.Button>
+          {tabItems.map((item, index) => (
+            <Disclosure key={`tab-item-${item.title}`} defaultOpen={index == 0 ? true : false}>
+              {({ open }) => (
+                <div className="grid md:grid-cols-3 xl:grid-cols-4 md:gap-4 xl:gap-12">
+                  <div className="md:col-span-1">
+                    <Disclosure.Button className={`${open ? 'bg-violet text-white px-6 md:px-3' : ''} w-full flex items-center rounded-full gap-4 md:max-lg:gap-2 py-3 md:py-2 transition-colors`}>
+                      <div className={`h-[60px] w-[60px] grid place-content-center rounded-full border ${open ? 'border-transparent' : 'border-purple'}`}>
+                        <ArrowDownIcon className={`${!open && '-rotate-90'} transition`} width={45} height={45} />
+                      </div>
+                      <span className={`text-normal ${open ? 'font-bold' : 'font-extrabold'}`}>{item.title}</span>
+                    </Disclosure.Button>
+                  </div>
 
-                      <Transition
-                        enter="transition duration-100 ease-out"
-                        enterFrom="transform scale-95 opacity-0"
-                        enterTo="transform scale-100 opacity-100"
-                        leave="transition duration-75 ease-out"
-                        leaveFrom="transform scale-100 opacity-100"
-                        leaveTo="transform scale-95 opacity-0"
-                      >
-                        <Disclosure.Panel className="pt-6 mb-8" static>
-                          <p className="font-extrabold mb-5">
-                            Nutrabiotics se basa en seis (6) principios fundamentales de la Medicina funcional que guían la creación de herramientas y de material didáctico:
-                          </p>
-                          <p className="font-normal mb-8">
-                            Comprensión de la individualidad bioquímica de cada ser humano, basada en las interacciones entre los conceptos de genética y carácter único del medio ambiente.
-                          </p>
-                          <Image className="w-full rounded-2xl aspect-square object-cover" src={TabAImg} alt="Tab A" />
-                        </Disclosure.Panel>
-                      </Transition>
-                    </>
-                  )}
-                </Disclosure>
-              ))}
-            </div>
-          </div>
+                  <div className="md:col-span-2 xl:col-span-3">
+                    <Transition
+                      enter="transition duration-100 ease-out"
+                      enterFrom="transform scale-95 opacity-0"
+                      enterTo="transform scale-100 opacity-100"
+                      leave="transition duration-75 ease-out"
+                      leaveFrom="transform scale-100 opacity-100"
+                      leaveTo="transform scale-95 opacity-0"
+                    >
+                      <Disclosure.Panel className="max-md:pt-6 mb-8 xl:pl-28" static>
+                        <p className="font-extrabold max-sm:leading-snug mb-5">
+                          Nutrabiotics se basa en seis (6) principios fundamentales de la Medicina funcional que guían la creación de herramientas y de material didáctico:
+                        </p>
+                        <p className="font-normal max-sm:leading-snug mb-8">
+                          Comprensión de la individualidad bioquímica de cada ser humano, basada en las interacciones entre los conceptos de genética y carácter único del medio ambiente.
+                        </p>
+                        <Image className="w-full md:h-[30vw] xl:h-[25vw] rounded-2xl max-md:aspect-square object-cover" src={TabAImg} alt="Tab A" />
+                      </Disclosure.Panel>
+                    </Transition>
+                  </div>
+                </div>
+              )}
+            </Disclosure>
+          ))}
         </div>
       </section>
 
-      <section className="py-8">
+      <section className="py-8 pb-20">
         <div className="container mx-auto">
           <h2 className="text-title text-center mb-10">
             <span className="text-subtitle">Un sueño</span> <br /> Hecho realidad
           </h2>
+          <p className="text-center lg:text-[32px] lg:leading-10 font-medium">
+            Hoy, ese sueño sigue intacto, haciendo visible nuestro objetivo en centenares de miembros de una tribu que se mueve hacia el mismo fin: <b className="font-extrabold">Reavivar los propósitos médicos, aportando bienestar al mundo.</b>
+          </p>
         </div>
       </section>
     </>
--- src/components/icons/ArrowDownIcon.jsx
+++ src/components/icons/ArrowDownIcon.jsx
@@ -3,6 +3,7 @@ const ArrowDownIcon = (props) => (
     xmlns="http://www.w3.org/2000/svg"
     width={56}
     height={56}
+    viewBox="0 0 56 56"
     fill="none"
     {...props}
   >
